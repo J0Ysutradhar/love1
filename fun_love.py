28e09dfc80ee157b6a530846fed404d3a06717cc
Joy3301 <91220470+Joy3301@users.noreply.github.com>
2022-10-25 09:42:39 +0600

    Add files via upload

---
+++ fun_love.py
@@ -0,0 +1,18 @@
+"""
+name = str(input("What is your name :   "))
+age = int(input("What is your age :      "))
+
+if age < 18 :
+	print (name  + " you are not an adult")
+elif age<=18 :
+	print (name + " you are an adult" )
+else:
+	print (name + " you are an adult")
+
+		"""
+l = str(input("your text  : "))
+t = int(input("How many time do you want write ? :  "))		
+i =   1
+while i<=t:
+	print((l   )  + str( i) )
+	i = i + 1
